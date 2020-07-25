65d0e2e27ebc46c56ab85e6928001b0917830ac8
radlee <prof.lolito@gmail.com>
2020-07-25 11:16:23 +0200

    Updates

---
+++ precache-manifest.5da86dbb94e2074ca2927fbdd7ae67db.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d67f1e4ba735e9b9661892a66d34095",
+    "url": "/origin/index.html"
+  },
+  {
+    "revision": "018721bdbcec0f99cc54",
+    "url": "/origin/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d1f776f1920c8be5ef8e",
+    "url": "/origin/static/css/main.077b2b84.chunk.css"
+  },
+  {
+    "revision": "018721bdbcec0f99cc54",
+    "url": "/origin/static/js/2.36365ef9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/origin/static/js/2.36365ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f776f1920c8be5ef8e",
+    "url": "/origin/static/js/main.6db2640c.chunk.js"
+  },
+  {
+    "revision": "dd23dab0709f125b7d08",
+    "url": "/origin/static/js/runtime-main.3ea35a32.js"
+  },
+  {
+    "revision": "2efb6a6dda5449acf2eaaec54d601a98",
+    "url": "/origin/static/media/coding2.2efb6a6d.jpg"
+  },
+  {
+    "revision": "5abea5314e1b6931e68b8b797bbeae7f",
+    "url": "/origin/static/media/logo512.5abea531.png"
+  },
+  {
+    "revision": "9e0378daf4df1a015c9dcd935ba0d314",
+    "url": "/origin/static/media/onepointone.9e0378da.jpg"
+  },
+  {
+    "revision": "aa2f28f45d4f6edbd8d9158ed215191c",
+    "url": "/origin/static/media/origin2020bg.aa2f28f4.jpg"
+  },
+  {
+    "revision": "48d21521abe64445c41f1c6f9bc06a33",
+    "url": "/origin/static/media/phone.48d21521.png"
+  }
+]);
